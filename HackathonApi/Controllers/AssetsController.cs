b116635650457ebd1f941ea3f1e8122639070136
Zackary Wiseman <zackary.wiseman@cuc.org>
2019-10-16 21:18:02 -0500

    getting rid of my changes

--- HackathonApi/Controllers/AssetsController.cs
+++ HackathonApi/Controllers/AssetsController.cs
@@ -11,13 +11,94 @@ using Microsoft.AspNetCore.Mvc;
 [Route("api/v1/[controller]")]
 public class AssetsController : ControllerBase
 {
+    static AssetList assetList;
     private readonly IMediator _mediator;
-    static AssetList _assetList;
+
+    static AssetsController()
+    {
+        assetList = new AssetList();
+        //assetList.Data = new List<Asset>
+        //{
+        //    new Asset
+        //    {
+        //        Id = Guid.NewGuid(),
+        //        AssetTag = "P0001",
+        //        Name = "Dell Latitude E7450",
+        //        OwnedBy = "Jane Doe",
+        //        Location = new Location
+        //        {
+        //            Id = Guid.NewGuid(),
+        //            Name = "New York Office",
+        //            Street = "1535 West 32nd Street",
+        //            City = "New York",
+        //            State = "NY",
+        //            Zip = "10101"
+        //        },
+        //        Status = "Active",
+        //        SupportGroup = "IT",
+        //        AssignmentGroup = "Marketing"
+        //    },
+        //    new Asset
+        //    {
+        //        Id = Guid.NewGuid(),
+        //        AssetTag = "P0002",
+        //        Name = "Dell 32 inch monitor",
+        //        OwnedBy = "Jane Doe",
+        //        Location = new Location
+        //        {
+        //            Name = "New York Office",
+        //            Street = "1535 West 32nd Street",
+        //            City = "New York",
+        //            State = "NY",
+        //            Zip = "10101"
+        //        },
+        //        Status = "Active",
+        //        SupportGroup = "IT",
+        //        AssignmentGroup = "Marketing"
+        //    },
+        //    new Asset
+        //    {
+        //        Id = Guid.NewGuid(),
+        //        AssetTag = "P0003",
+        //        Name = "Logitech Keyboard",
+        //        OwnedBy = "Jane Doe",
+        //        Location = new Location
+        //        {
+        //            Name = "New York Office",
+        //            Street = "1535 West 32nd Street",
+        //            City = "New York",
+        //            State = "NY",
+        //            Zip = "10101"
+        //        },
+        //        Status = "Active",
+        //        SupportGroup = "IT",
+        //        AssignmentGroup = "Marketing"
+        //    },
+        //    new Asset
+        //    {
+        //        Id = Guid.NewGuid(),
+        //        AssetTag = "P0001",
+        //        Name = "Logitech Wireless Mouse",
+        //        OwnedBy = "Jane Doe",
+        //        Location = new Location
+        //        {
+        //            Name = "New York Office",
+        //            Street = "1535 West 32nd Street",
+        //            City = "New York",
+        //            State = "NY",
+        //            Zip = "10101"
+        //        },
+        //        Status = "Active",
+        //        SupportGroup = "IT",
+        //        AssignmentGroup = "Marketing"
+        //    }
+        //};
+
+    }
 
     public AssetsController(IMediator mediator)
     {
         _mediator = mediator;
-        _assetList = new AssetList();
     }
 
     /// <summary>
@@ -47,6 +128,7 @@ public class AssetsController : ControllerBase
     [ProducesResponseType(404, Type = typeof(void))]
     public async Task<IActionResult> GetAsset([FromRoute] string assetTag)
     {
+        //var asset = assetList.Data.FirstOrDefault(a => a.AssetTag == assetTag);
         var asset = await _mediator.Send(new GetAssetRequest(assetTag));
         if (asset == null)
         {

b8362e622e041c95f8356396e6092b4e627ba2de
Zackary Wiseman <zackary.wiseman@cuc.org>
2019-10-16 23:09:02 -0500

    adding tests

--- HackathonTest/Tests/GetAssetHandlerTest.cs
+++ HackathonTest/Tests/GetAssetHandlerTest.cs
@@ -1,14 +1,32 @@
-using System;
+using HackathonApi.Models.Options;
 using Xunit;
+using Moq;
+using HackathonApi.Mediator;
+using MediatR;
+using System.Threading;
 
 namespace HackathonTest
 {
     public class GetAssetHandlerTest
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly string _host = "http://moq.this.gov";
+        private readonly string _user = "HelloWorld";
+        private readonly string _secret = "HelloSecret";
+        private ServiceNowOptions MockServiceNowSetup() {
+            var options = new Mock<ServiceNowOptions>();
+            options.Setup(m => m.ServiceNowHost).Returns(_host);
+            options.Setup(m => m.ServiceNowUser).Returns(_user);
+            options.Setup(m => m.ServiceNowSecret).Returns(_secret);
+            return options.Object;
+        }
 
+        [Fact]
+        public void GetAssetHandler()
+        {
+            var mockMediator = new Mock<IMediator>();
+            mockMediator.Setup(s => s.Send(It.IsAny<GetAssetRequest>(), default(CancellationToken))).Verifiable("Notification was not sent.");
+            Assert.NotNull(mockMediator);
+            Assert.NotNull(mockMediator.Object);
         }
     }
 }
